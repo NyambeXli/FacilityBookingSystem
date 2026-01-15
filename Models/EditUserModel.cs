e0bd85d01b0945ff232f78b7d97304236c00e9a4
NyambeXli <lxnyambe@gmail.com>
2026-01-15 11:47:50 +0200

    Add files via upload

---
+++ Models/EditUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class EditUserModel
+{
+    public string UserId { get; set; }
+
+    [Required(ErrorMessage = "The First Name field is required.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "The Last Name field is required.")]
+    public string Surname { get; set; }
+
+    [Required(ErrorMessage = "The Email Address field is required.")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+    public string EmailAddress { get; set; }
+
+    [Required(ErrorMessage = "The UFS Student Number field is required.")]
+    public string Identification { get; set; }
+}
---
+++ Models/Library.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UfsConnectBook.Models
+{
+    public class Library
+    {
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Please enter your surname.")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter your ID.")]
+        public string LibraryName { get; set; }
+        [Required(ErrorMessage = "Please enter the library name.")]
+        public DateTime CheckInTime { get; set; }
+        [Required(ErrorMessage = "Please enter the time of geting into the library.")]
+        public DateTime CheckOutTime { get; set; }
+        [Required(ErrorMessage = "Please enter the time of geting out of the library.")]
+        public int NumberOfParticipants { get; set; }
+        [Required(ErrorMessage = "Please enter the number of participants in the discussion.")]
+
+        public int LibrayNameId { get; set; }
+       
+       
+
+    }
+}
