8e6bb1fa0bec1baef6f4df2305710628965146b9
NyambeXli <lxnyambe@gmail.com>
2026-01-15 11:52:56 +0200

    Add files via upload

---
+++ Models/Entities/FeedBack.cs
@@ -0,0 +1,11 @@
+﻿namespace UfsConnectBook.Models.Entities
+{
+    public class FeedBack
+    {
+        public int Id { get; set; }
+
+        public string Comment { get; set; }
+
+        public int Number { get; set; }
+    }
+}
