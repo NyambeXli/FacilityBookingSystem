1dd4668354b78bbff40fc568e9e28a8da39ec03a
NyambeXli <lxnyambe@gmail.com>
2026-01-15 14:15:43 +0200

    Add files via upload

---
+++ HelperClass/PdfGenerator.cs
@@ -0,0 +1,44 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using System.IO;
+
+namespace UfsConnectBook.Utilities
+{
+    public class PdfGenerator : IPdfGenerator
+    {
+        private readonly IConverter _converter;
+
+        public PdfGenerator(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public byte[] GeneratePdf(string htmlContent)
+        {
+            var globalSettings = new GlobalSettings
+            {
+                PaperSize = PaperKind.A4,
+                Orientation = Orientation.Portrait,
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = htmlContent,
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            return _converter.Convert(pdf);
+        }
+    }
+
+    public interface IPdfGenerator
+    {
+        byte[] GeneratePdf(string htmlContent);
+    }
+}
